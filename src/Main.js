a417700c19982322f7c6368b6eb8759c9b0809b2
Grace Brackman <gbrackma@purdue.edu>
2018-05-23 10:42:15 -0400

    store notes in main's state

--- src/Main.js
+++ src/Main.js
@@ -4,14 +4,40 @@ import Sidebar from './Sidebar'
 import NoteList from './NoteList'
 import NoteForm from './NoteForm'
 
-const Main = () =>{
-    return (
-        <div className='Main' style={style}>
-        <Sidebar />
-        <NoteList />
-        <NoteForm />
-        </div>
-    )
+class Main extends React.Component {
+
+    constructor() {
+        super()
+        this.state = {
+            notes: [
+                {
+                    id: 1,
+                    title: 'why i <3 js',
+                    body: 'bc i like to code',
+                },
+                {
+                    id: 2,
+                    title: 'hello',
+                    body: 'this is hello text',
+                },
+                {
+                    id: 3,
+                    title: 'notenotenote',
+                    body: 'body body body',
+                },
+        
+            ]
+        }
+    }
+    render() {
+        return (
+            <div className='Main' style={style} >
+                <Sidebar />
+                <NoteList notes={this.state.notes}/>
+                <NoteForm />
+            </div>
+        )
+    }
 }
 
 const style = {
--- src/NoteList.js
+++ src/NoteList.js
@@ -3,31 +3,14 @@ import Note from './Note'
 
 import './NoteList.css'
 
-const NoteList = () =>{
-    const notes = [
-        {
-            id: 1,
-            title: 'why i <3 js',
-            body: 'bc i like to code',
-        },
-        {
-            id: 2,
-            title: 'hello',
-            body: 'this is hello text',
-        },
-        {
-            id: 3,
-            title: 'notenotenote',
-            body: 'body body body',
-        },
-
-    ]
+const NoteList = (props) =>{
+    
     return (
         <div className="NoteList">
             <h3>Notes</h3>
             <ul id="notes">
                 <a className="active">
-                    {notes.map(currentItem => <Note key={currentItem.id} note={currentItem}/>)}
+                    {props.notes.map(currentItem => <Note key={currentItem.id} note={currentItem}/>)}
                 </a>
             </ul>
         </div>
