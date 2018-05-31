d2962aa18f1b31b1cb6440a789b813f22af9abdf
Grace Brackman <gbrackma@purdue.edu>
2018-05-31 09:19:32 -0400

    use component did update rather than componenet did recience props

--- src/NoteForm.js
+++ src/NoteForm.js
@@ -10,21 +10,21 @@ class NoteForm extends Component {
     }
   }
 
-  componentWillReceiveProps = (newProps) => {
-    // Get the ID from the URL
-    const newId = newProps.match.params.id
 
-    // Find the note with that ID
-    //console.log(newProps.notes)
-    const i = newProps.notes.findIndex(currentNote => currentNote.id.toString() === newId)
-    const note = newProps.notes[i] || this.blankNote()
+  componentDidUpdate = () => {
+    const newId = this.props.match.params.id || ""
 
-    //console.log(i)
+    const oldId = this.state.note.id || ""
 
-    // Update state with that note, if found
-    if (note) {
-      this.setState({ note })
+    if (newId !== oldId.toString()) {
+      const i = this.props.notes.findIndex(currentNote => currentNote.id.toString() === newId)
+      const note = this.props.notes[i] || this.blankNote()
+
+      if (note.id !== this.state.note.id) {
+        this.setState({ note })
+      }
     }
+
   }
 
   blankNote = () => {
@@ -32,7 +32,7 @@ class NoteForm extends Component {
       id: null,
       title: '',
       body: '',
-      updatedAt: '',
+      //updatedAt: '',
     }
   }
 
@@ -40,9 +40,9 @@ class NoteForm extends Component {
     const note = { ...this.state.note }
     note[ev.target.name] = ev.target.value
 
-    var today = new Date()
-    note.updatedAt = today.getMonth() + '/' + (today.getDate()) + '/' + today.getFullYear() + ' at ' + today.getHours() + ':' + today.getUTCMinutes()
-    console.log(note)
+    //var today = new Date()
+    //note.updatedAt = today.getMonth() + '/' + (today.getDate()) + '/' + today.getFullYear() + ' at ' + today.getHours() + ':' + today.getUTCMinutes()
+    //console.log(note)
     this.setState(
       { note },
       () => (this.props.saveNote(note))
@@ -72,7 +72,7 @@ class NoteForm extends Component {
             />
           </p>
 
-          <h3>Time last updated: {this.state.note.updatedAt}</h3>
+          <h3>Time last updated:</h3>
 
           <textarea
             name="body"
